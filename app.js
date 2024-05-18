a810a587217da3fd259ec5cf7d810610c4d7d0c0
carissa-chandra <carissa.tjan03@gmail.com>
2024-05-18 13:19:57 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is up and running on ${PORT} ...`);
+});
