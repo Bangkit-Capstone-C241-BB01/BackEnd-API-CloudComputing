877fb3f629e55c2a29e1f3eca68a927606b43fbd
carissa-chandra <carissa.tjan03@gmail.com>
2024-05-26 16:02:59 +0700

    Initiate Routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const { userSignUp, userLogin } = require('../controllers/authentication');
+const { authenticateToken } = require('../middleware/middleware');
 
 const router = express.Router();
 
-//TODO: Authentication (POST register & login for 3 roles admin, seller, & user)
+//Authentication
 router.post('/register', userSignUp);
 router.post('/login', userLogin);
 
+router.get('/profile', ); //get detail profile
+router.put('/profile', ); //update profile
+
+router.get('/stores', ); // get all stores (optional)
+router.get('/stores/:store_id', ); //detail store
+router.put('/stores/:store_id', ); //update store
+
+router.get('/products', ); //get all products
+router.get('/products/:product_id', ); //detail product
 
 module.exports = { router };
